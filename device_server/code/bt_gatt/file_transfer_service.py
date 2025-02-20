a56fb4fd0be14a10ede0fafce1f8127ea16632f8
Anson Cheng <Anson.12666@icloud.com>
2025-02-20 16:13:00 +0800

    fix: wrong file name

--- device_server/code/bt_gatt/file_transfer_service.py
+++ device_server/code/bt_gatt/file_transfer_service.py
@@ -1,59 +1,65 @@
 from bt_gatt.service import Service, Characteristic
-from motor_control import handle_command_input
 import dbus
 from bt_gatt.constants import GATT_CHRC_IFACE
 import bt_gatt.exceptions as exceptions
+import logging
 
-class MotorService(Service):
+logging.basicConfig(filename='file_transfer.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+class FileTransferService(Service):
     """
-    Motor Service with read and write characteristics only.
+    File Transfer Service with read and write characteristics.
     """
-    MOTOR_UUID = '0000180d-0000-1000-8000-00805f9b34fb'
+    FILE_TRANSFER_UUID = '0000180e-0000-1000-8000-00805f9b34fb'
 
     def __init__(self, bus, index):
-        Service.__init__(self, bus, index, self.MOTOR_UUID, True)
-        self.add_characteristic(MotorReadChrc(bus, 0, self))
-        self.add_characteristic(MotorWriteChrc(bus, 1, self))
-        self.motor_status = 0
-        self.data_buffers = {}  # Buffer to accumulate data from different clients
+        Service.__init__(self, bus, index, self.FILE_TRANSFER_UUID, True)
+        self.add_characteristic(FileReadChrc(bus, 0, self))
+        self.add_characteristic(FileWriteChrc(bus, 1, self))
+        self.file_data = {}  # Dictionary to store file data from different clients
+        logging.info("FileTransferService initialized")
 
 
-class MotorReadChrc(Characteristic):
-    MOTOR_READ_UUID = '00002a37-0000-1000-8000-00805f9b34fb'
+class FileReadChrc(Characteristic):
+    FILE_READ_UUID = '00002a3a-0000-1000-8000-00805f9b34fb'
 
     def __init__(self, bus, index, service):
         Characteristic.__init__(
                 self, bus, index,
-                self.MOTOR_READ_UUID,
+                self.FILE_READ_UUID,
                 ['read'],
                 service)
+        logging.info("FileReadChrc initialized")
 
     def ReadValue(self, options):
-        # Return current motor status.
-        return [self.service.motor_status]
+        client_address = options.get('client_address', 'default')
+        file_data = self.service.file_data.get(client_address, b'')
+        logging.info(f"Read request from {client_address}, data: {file_data}")
+        return list(file_data)
 
 
-class MotorWriteChrc(Characteristic):
-    MOTOR_WRITE_UUID = '00002a39-0000-1000-8000-00805f9b34fb'
+class FileWriteChrc(Characteristic):
+    FILE_WRITE_UUID = '00002a3b-0000-1000-8000-00805f9b34fb'
 
     def __init__(self, bus, index, service):
         Characteristic.__init__(
                 self, bus, index,
-                self.MOTOR_WRITE_UUID,
+                self.FILE_WRITE_UUID,
                 ['write'],
                 service)
+        logging.info("FileWriteChrc initialized")
 
     def WriteValue(self, value, options):
-        command = ''.join(chr(b) for b in value).strip()
         client_address = options.get('client_address', 'default')
-        handle_command_input(command)
-        print(f"Received command from {client_address}: {command}")
-
-        if len(value) == 1:
-            byte = value[0]
-            print('Motor control value: ' + repr(byte))
-
-            self.service.motor_status = byte
-            print('Motor status updated to: ' + str(self.service.motor_status))
-        else:
-            raise exceptions.InvalidValueLengthException()
+        file_content = bytes(value)
+        
+        if client_address not in self.service.file_data:
+            self.service.file_data[client_address] = b''
+        
+        self.service.file_data[client_address] += file_content
+        logging.info(f"Received file data from {client_address}: {file_content}")
+        print(f"Received file data from {client_address}: {file_content}")
+        
+        with open(f"received_{client_address}.bin", "ab") as f:
+            f.write(file_content)
+        logging.info(f"File data saved for {client_address}")
