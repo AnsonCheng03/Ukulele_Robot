9e894f375ec7504b49b472140321adfa89a2672e
Anson Cheng <Anson.12666@icloud.com>
2025-02-20 16:14:22 +0800

    chore: temp disable i2c

--- device_server/code/main.py
+++ device_server/code/main.py
@@ -5,12 +5,12 @@ from utils import manual_input_handler
 from motor_control import slaves
 
 # Initial scan and configuration
-found_devices = scan_i2c()
-print("Found devices: %s" % found_devices)
+# found_devices = scan_i2c()
+# print("Found devices: %s" % found_devices)
 
-for address, motor in slaves.items():
-    if address in found_devices:
-        receive_motor_config(address, motor)
+# for address, motor in slaves.items():
+#     if address in found_devices:
+#         receive_motor_config(address, motor)
 
 # Start Bluetooth server for command input
 bluetooth_thread = threading.Thread(target=start_bluetooth_server, daemon=True)
