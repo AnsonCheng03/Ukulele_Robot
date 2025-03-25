86ace0f2e5c96fb0ff1b6bb07c53232a307bc5ef
Anson Cheng <Anson.12666@icloud.com>
2025-03-26 03:31:09 +0800

    feat: implement CommandProcessor for handling motor commands and improve command processing structure

--- Uno_board/Uno_board.ino
+++ Uno_board/Uno_board.ino
@@ -4,6 +4,7 @@
 #include "FingeringMotor.h"
 #include "JobQueue.h"
 #include "Fingering.h"
+#include "CommandProcessor.h"
 
 #define CMD_CONTROL 0
 #define CMD_CALIBRATE 1
@@ -128,177 +129,14 @@ void handleSerialInput() {
     while (Serial1.available() > 0) {
         char incomingByte = Serial1.read();
 
-        // Echo to USB serial
-        // Serial.print("Received: ");
-        // Serial.println(incomingByte);
-
         if (incomingByte == '\n') {
             inputLine.trim();
             if (inputLine.length() > 0) {
-                processCommand(inputLine);
+                processCommand(inputLine, sliders, rackMotors, fingeringMotors);
             }
             inputLine = "";
         } else {
             inputLine += incomingByte;
         }
-    }
-}
-
-void processCommand(const String& commandStr) {
-    Serial.println("Processing: " + commandStr);
-    char* tokens[8];
-    char buffer[100];
-    commandStr.toCharArray(buffer, sizeof(buffer));
-    
-    int tokenCount = 0;
-    char* token = strtok(buffer, " ");
-    while (token != nullptr && tokenCount < 8) {
-        tokens[tokenCount++] = token;
-        token = strtok(nullptr, " ");
-    }
-
-    if (tokenCount == 0) return;
-
-    String cmd = tokens[0];
-    if (cmd == "control" && tokenCount == 6) {
-        int motorID = atoi(tokens[1]);
-        int target = atoi(tokens[2]);
-        int speed = atoi(tokens[3]);
-        int direction = atoi(tokens[4]);
-        unsigned long duration = atol(tokens[5]);
-
-        Serial.print("CMD_CONTROL: MotorID=");
-        Serial.print(motorID);
-        Serial.print(", Target=");
-        Serial.print(target);
-        Serial.print(", Speed=");
-        Serial.print(speed);
-        Serial.print(", Direction=");
-        Serial.print(direction);
-        Serial.print(", Duration=");
-        Serial.println(duration);
-
-        if (motorID == 0) {
-            if(target == 0 || target == 1)
-                for (int i = 0; i < 4; ++i) {
-                    sliders[i].control(direction, speed, duration);
-                }
-            if(target == 0 || target == 2)
-                for (int i = 0; i < 4; ++i) {
-                    rackMotors[i].control(direction, speed, duration);
-                }
-            if(target == 0 || target == 3)
-                for (int i = 0; i < 4; ++i) {
-                    fingeringMotors[i].control(direction, speed, duration);
-                }
-        } else if (motorID <= 4) {
-            if (target == 0 || target == 1)
-                sliders[motorID - 1].control(direction, speed, duration);
-            if (target == 0 || target == 2)
-                rackMotors[motorID - 1].control(direction, speed, duration);
-            if (target == 0 || target == 3)
-                fingeringMotors[motorID - 1].control(direction, speed, duration);
-        }
-    } else if (cmd == "C" && tokenCount == 3) {
-        int motorID = atoi(tokens[1]);
-        int target = atoi(tokens[2]);
-
-        Serial.print("CMD_CALIBRATE: MotorID=");
-        Serial.print(motorID);
-        Serial.print(", Target=");
-        Serial.print(target);
-
-        if (motorID == 0) {
-            if (target == 0 || target == 1)
-                for (int i = 0; i < 4; ++i) {
-                    sliders[i].calibrate();
-                }
-            if (target == 0 || target == 2)
-                for (int i = 0; i < 4; ++i) {
-                    rackMotors[i].calibrate();
-                }
-            if (target == 0 || target == 3)
-                for (int i = 0; i < 4; ++i) {
-                    fingeringMotors[i].calibrate();
-                }
-        } else if (motorID <= 4) {
-            if (target == 0 || target == 1)
-                sliders[motorID - 1].calibrate();
-            if (target == 0 || target == 2)
-                rackMotors[motorID - 1].calibrate();
-            if (target == 0 || target == 3)
-                fingeringMotors[motorID - 1].calibrate();
-        }
-    } else if (cmd == "M" && tokenCount == 4) {
-        int motorID = atoi(tokens[1]);
-        int target = atoi(tokens[2]);
-        int distance = atoi(tokens[3]);
-
-        Serial.print("CMD_MOVE: MotorID=");
-        Serial.print(motorID);
-        Serial.print(", Target=");
-        Serial.print(target);
-        Serial.print(", Distance=");
-        Serial.println(distance);
-
-        if (motorID == 0) {
-            if (target == 0 || target == 1)
-                for (int i = 0; i < 4; ++i) {
-                    sliders[i].move(distance);
-                }
-            if (target == 0 || target == 2)
-                for (int i = 0; i < 4; ++i) {
-                    rackMotors[i].move(distance);
-                }
-            if (target == 0 || target == 3)
-                for (int i = 0; i < 4; ++i) {
-                    fingeringMotors[i].move(distance);
-                }
-        } else if (motorID <= 4) {
-            // TODO: add fingering motor
-            if (target == 0)
-                moveFinger(sliders[motorID - 1], rackMotors[motorID - 1], distance);
-            else if (target == 1)
-                sliders[motorID - 1].move(distance);
-            else if (target == 2)
-                rackMotors[motorID - 1].move(distance);
-            else if (target == 3)
-                fingeringMotors[motorID - 1].move(distance);
-        }
-    } else if (cmd == "D" && tokenCount == 4) {
-        int motorID = atoi(tokens[1]);
-        int target = atoi(tokens[2]);
-        int position = atoi(tokens[3]);
-
-        Serial.print("CMD_DEBUG: MotorID=");
-        Serial.print(motorID);
-        Serial.print(", Target=");
-        Serial.print(target);
-        Serial.print(", Position=");
-        Serial.println(position);
-
-        if (motorID == 0) {
-            if (target == 0 || target == 1)
-                for (int i = 0; i < 4; ++i) {
-                    sliders[i].moveBy(position);
-                }
-            if (target == 0 || target == 2)
-                for (int i = 0; i < 4; ++i) {
-                    rackMotors[i].moveBy(position);
-                }
-            if (target == 0 || target == 3)
-                for (int i = 0; i < 4; ++i) {
-                    fingeringMotors[i].moveBy(position);
-                }
-        } else if (motorID <= 4) {
-            if (target == 0 || target == 1)
-                sliders[motorID - 1].moveBy(position);
-            if (target == 0 || target == 2)
-                rackMotors[motorID - 1].moveBy(position);
-            if (target == 0 || target == 3)
-                fingeringMotors[motorID - 1].moveBy(position);
-        }
-    } else {
-        Serial.println("Unknown or invalid command.");
     }
 }
