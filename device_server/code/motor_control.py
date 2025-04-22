442d2425fd39a02d05d3a71849e4e542ecb6c4b6
Anson Cheng <Anson.12666@icloud.com>
2025-04-22 08:14:45 +0800

    chore: change distance to arg

--- device_server/code/motor_control.py
+++ device_server/code/motor_control.py
@@ -168,12 +168,11 @@ def send_motor_command(motor_id, command_type, *args):
             msg = f"D {motor_id} {target} {position_mm}\n"
             
         elif command_type == 6:  # Multi-finger MF command
-            distances = args[0]  # should be a list of 4 ints or -1
-            if len(distances) != 4:
-                print("MF command requires exactly 4 distances, currently: ", distances)
+            if len(args) != 4:
+                print("MF command requires exactly 4 distances, currently: ", args)
                 return
 
-            msg = f"MF {' '.join(str(d) for d in distances)}\n"
+            msg = f"MF {' '.join(str(d) for d in args)}\n"
 
         else:
             print("Unsupported command type")
